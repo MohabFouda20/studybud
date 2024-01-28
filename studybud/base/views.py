88fa498cf01200c30f7d4561a952f4f5c7f5e301
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2024-01-28 23:30:35 +0200

    begin with tamplate

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'home.html' )
 
 
 
 def room(request):
-    return HttpResponse('Room')
+    return render(request,'room.html')
 # Create your views here.
