16ea208a7905fe1774035b859dc91c7c3512aafa
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2024-01-29 03:10:21 +0200

    finish temp without being in the app

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+
+rooms =[
+    {'id':1 , 'name':'lets learn python'},
+    {'id':2 , 'name':'lets learn js'},
+    {'id':3 , 'name':'lets learn php'},
+    {'id': 4 , 'name' : 'what the heelll'},
+]
 
 def home(request):
-    return render(request, 'home.html' )
+    return render(request, 'home.html' , {'rooms' : rooms } )
 
 
 
-def room(request):
+def room(request,pk):
     return render(request,'room.html')
 # Create your views here.
