b24647dfcd2097b32eba729def4ccf0ce322e08e
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2024-02-01 11:51:05 +0200

    msh faker 3amalt eh

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -3,26 +3,26 @@ from .models import Room
 # from django.http import HttpResponse
 
 
-rooms =[
-    {'id':1 , 'name':'lets learn python'},
-    {'id':2 , 'name':'lets learn js'},
-    {'id':3 , 'name':'lets learn php'},
-    {'id': 4 , 'name' : 'what the heelll'},
-]
+# rooms =[
+#     {'id':1 , 'name':'lets learn python'},
+#     {'id':2 , 'name':'lets learn js'},
+#     {'id':3 , 'name':'lets learn php'},
+#     {'id': 4 , 'name' : 'what the heelll'},
+# ]
 
 def home(request):
-    rooms = Room.objects
+    rooms = Room.objects.all()
     context ={'rooms':rooms}
     return render(request, 'base/home.html' , context )
 
 
 
 def room(request,pk):
-    room =None
-    for i in rooms:
-        if i['id'] == int (pk):
-            room =i
-        context= {'rooms':rooms}
+    room =Room.objects.get(id=pk)
+    # for i in rooms:      # before the database and models 
+    #     if i['id'] == int (pk):
+    #         room =i
+    context= {'room':room}
     
     
     return render(request,'base/room.html', context)
