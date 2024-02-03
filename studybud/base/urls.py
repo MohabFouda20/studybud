231d3670be5d3d49ef28f953a4140f964ae5fe84
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2024-02-03 16:41:21 +0200

    finnaly solve the error of the html file and start with crud

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home , name= 'home'),
     path ('room/<str:pk>/', views.room , name = 'room'),
-    path('CreateRoom/', views.CreateRoom, name='create_room'),
+    path('CreateRoom/', views.CreateRoom, name='create-room'),
+    path('updateRoom/<str:pk>/', views.updateRoom, name='update-room'),
+    path('deleteRoom/<str:pk>/', views.deleteRoom, name='delete-room'),
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room
+from .forms import RoomForm
 # from django.http import HttpResponse
 
 
@@ -28,5 +29,30 @@ def room(request,pk):
     return render(request,'base/room.html', context)
 # Create your views here.
 def CreateRoom(request):
-    context={}
-    return render(request , 'base/room-form.html',context)
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request , pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method =='POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request , 'base/room_form.html', context)
+
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    if request.method=='POST':
+        room.delete()
+        return redirect('home')
+    return render(request ,'base/delete.html', {'obj':room})
